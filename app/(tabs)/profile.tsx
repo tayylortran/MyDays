27cc54232229e3834d8856a3393d8a8dea5de622
tayylortran <taytran04@gmail.com>
2026-08-13 15:31:08 -0700

    temp profile view top

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -2,13 +2,36 @@ import { MonthHeader } from '@/src/features/calendar/MonthHeader';
 import { ChooseDayFaceModal } from '@/src/features/profile/ChooseDayFaceModal';
 import { ProfileCalendarGrid } from '@/src/features/profile/ProfileCalendarGrid';
 import { useProfileScreen } from '@/src/features/profile/useProfileScreen';
-import { View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function Profile() {
   const profile = useProfileScreen();
 
   return (
     <View style={{ flex: 1, paddingTop: 70, paddingHorizontal: 8 }}>
+
+      <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          gap: 12,
+          marginBottom: 60,
+        }}
+      >
+        <View
+          style={{
+            width: 90,
+            height: 90,
+            borderRadius: 45,
+            backgroundColor: '#ddd',
+          }}
+        />
+
+        <Text style={{ fontSize: 20, fontWeight: '600' }}>
+          Profile user
+        </Text>
+      </View>
+
       <MonthHeader
         month={profile.month}
         year={profile.year}
