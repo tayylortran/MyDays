21069444d2214fa36842721c118bf6ae6f08c280
tayylortran <taytran04@gmail.com>
2026-09-20 14:32:53 -0700

    add friends tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -21,6 +21,13 @@ export default function TabsLayout() {
         }}
       />
       <Tabs.Screen
+        name="friends"
+        options={{
+          title: 'Friends',
+          tabBarIcon: ({ color, size }) => <Ionicons name="people-outline" color={color} size={size} />,
+        }}
+      />
+      <Tabs.Screen
         name="profile"
         options={{
           title: 'Profile',
